5b42297cec5fb4c8f1558cb8733ae3cf90412062
L0G1C06 <justgithub70@gmail.com>
2026-03-03 20:38:49 -0300

    feature: modelagem IQR, Z-Score, MAD

--- pipeProposal.py
+++ pipeProposal.py
@@ -14,6 +14,7 @@ Avaliar clusters específicos (órgão, ano, função)
 """
 
 import numpy as np
+from scipy.stats import median_abs_deviation
 
 def calculateQuartisIQR(conjunto: list):
     q1 = np.percentile(conjunto, 25)
@@ -26,12 +27,34 @@ def iqrOutlier(value, limits: list):
     inferior, superior = limits
     return 1 if(value < inferior or value > superior) else 0
 
+def zScore(conjunto: list):
+    data_mean = np.mean(np.array(conjunto))
+    data_std = np.std(np.array(conjunto))
+
+    z_scores = (conjunto - data_mean) / data_std
+
+    return z_scores
+
+def madOutliers(conjunto: list, threshold=3.5):
+    data = np.array(conjunto)
+    mediana = np.median(data)
+    mad = median_abs_deviation(data)
+
+    # Evitar divisão por zero
+    if mad == 0:
+        return np.zeros(len(data)), []
+
+    modified_z_scores = 0.6745 * (data - mediana) / mad
+
+    mask = np.abs(modified_z_scores) > threshold
+
+    return modified_z_scores, data[mask]
+
 if __name__ == "__main__":
     conjunto = [2, 5, 7, 10, 12, 15, 18, 20, 22, 24, 26, 100]
+    print(f"Conjunto: {conjunto}")
     log_conjunto = np.log10(conjunto)
     iqr, limits = calculateQuartisIQR(log_conjunto)
-    print("IQR:", iqr)
-    print("Limites:", limits)
     
     outliers = []
     normal_values = []
@@ -44,5 +67,19 @@ if __name__ == "__main__":
         else:
             normal_values.append(i)
 
-    print("Outliers:", outliers)
     print("Normal values:", normal_values)
+
+    print("Outliers IQR:", outliers)
+
+    z_scores = zScore(conjunto)
+
+    z_score_outliers = []
+    valores_outliers = []
+
+    mask = np.abs(z_scores) > 3
+    valores_outliers = np.array(conjunto)[mask]
+    print("Valores outliers Z Scores:", valores_outliers.tolist())
+
+    modified_z, mad_outliers = madOutliers(conjunto)
+
+    print("Valores outliers (MAD):", mad_outliers.tolist())
